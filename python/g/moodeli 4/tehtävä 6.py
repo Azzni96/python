810b11f6ad8bce20502b5733bda11dc6e72a3fda
azzni96 <nihadazzam96@gmail.com>
2023-09-03 17:02:59 +0300

    testi

---
+++ python/g/moodeli 4/tehtävä 6.py
@@ -0,0 +1,14 @@
+import random
+total_points = int(input("Total_points: "))
+points_generated =""
+points_inside_circle = ""
+
+while total_points != 0:
+     x = random.randint(-1,1)
+     y = random.randint(-1,1)
+     if (x^2+y^2) >1:
+      points_inside_circle = int(input("points_inside_ciecle: "))
+     print(points_inside_circle)
+
+     if points_generated == 4 * (points_inside_circle) / (total_points):
+      print(points_generated)
