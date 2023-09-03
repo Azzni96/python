d5fd58afc1ae80a8a312fb2fdfec473743b98a71
azzni96 <nihadazzam96@gmail.com>
2023-09-03 17:25:05 +0300

    testi

--- python/g/moodeli 4/tehtävä 5.py
+++ python/g/moodeli 4/tehtävä 5.py
@@ -1,15 +1,12 @@
 user = ""
 password = ""
 person = 0
-user = input("Anna user: ")
-password = input("Anna password: ")
 while user != 'python' and password != 'rules':
-       print("Write again.")
-       if person <= 5:
-        user = input("Anna user: ")
-        password = input("Anna password: ")
-        break
-        person +=1
-        print("Write again.")
+     if person < 5:
+      user = input("Anna user: ")
+      password = input("Anna password: ")
+      print("Write again.")
+      person += 1
+
 print("Access denied.")
 print("Welcome.")
