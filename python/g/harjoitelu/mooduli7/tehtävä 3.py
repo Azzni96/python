efd2efbeeb2e4bfb83e171fddf64d2084ac591d1
azzni96 <nihadazzam96@gmail.com>
2023-09-12 06:44:57 +0300

    testi

--- python/g/harjoitelu/mooduli7/tehtävä 3.py
+++ python/g/harjoitelu/mooduli7/tehtävä 3.py
@@ -1,16 +1,39 @@
-lentoasemat = {"helsinki-vantaa": "EFHK",
-               "helsinki-malmi": "EFHF",
-               "jyväskylä": "EFJY",
-               "kajaani": "EFKI",
-               "forssa": "EFFO"}
-lentoasema = ""
-
+lentoasemat = {"EFHK" : "helsinki-vantaa lentoasema",
+               "EFHF":"helsinki-malmi lentoasema",
+               "EFJY":"jyväskylä lentoasema",
+               "EFKI":"kajaani lentoasema",
+               "EFFO":"forssa lentoasema"}
 while True:
-    lentoasema = input("anna lentoasema: ")
-    if lentoasema in lentoasemat:
-        print(f"lentoasema {lentoasema} ICAO-koodi on {lentoasemat[lentoasema]}.")
-    elif lentoasema == "lopettaa":
+    päätös = input("""
+uusi lentoasema: 
+hakea lentoasema: 
+lopeta: 
+  """)
+    if päätös == "uusi":
+      koodi = input("Anna ICAO-koodi: ")
+      kenttä = input("Anna lentoasema: ")
+      if koodi in lentoasemat:
+       print("koodi on jo olemassa. ")
+      else:
+       lentoasemat[koodi] = kenttä
+       print("uusi lentokenttä lisätty!")
+
+
+    elif päätös == "hakea":
+        koodi = input("Anna ICAO-koodi: ")
+        if koodi in lentoasemat:
+         print(f"ICAO-koodi {koodi} on {lentoasemat[koodi]}")
+        if koodi not in lentoasemat:
+         print("valitettavasti, koodi ei löytää, yritä uudelleen tai lisää sitä. ")
+         päätös = input("""
+uusi lentoasema: 
+hakea lentoasema: 
+lopeta: 
+""")
+    elif päätös == "lopeta":
         break
+    else:
+        print("Anteeksi, en ymmärtää mitä haluat, yritä uudelleen")
 
-
+print("lopettaa")
 
