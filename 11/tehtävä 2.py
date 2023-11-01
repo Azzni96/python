36861818d96b59072e59133feb760c5e488d531e
azzni96 <nihadazzam96@gmail.com>
2023-11-01 20:59:44 +0200

    v

--- 11/tehtävä 2.py
+++ 11/tehtävä 2.py
@@ -1,31 +1,40 @@
 class Auto:
-    def __init__(self, rekisterinimi, huipponopeus):
-        self.nimi = rekisterinimi
-        self.nopeus = huipponopeus
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.matkamittari = 0
 
-    def tulosta_tiedot(self):
-        print(f"{self.nimi}, {self.nopeus} km/h")
+    def aja(self, tuntimaara):
+        self.matkamittari += self.huippunopeus * tuntimaara
 
-class sähköauto(Auto):
-    def __init__(self,rekisterinimi, huipponopeus, akkukapasiteetti):
-        self.akku = akkukapasiteetti
-        super().__init__(rekisterinimi, huipponopeus)
-    def tulosta_tiedot(self):
-        super().tulosta_tiedot()
-        print(f"{self.akku} kWh")
-class polttomoottoriauto(Auto):
-    def __init__(self,rekisterinimi, huipponopeus, bensatankin):
-        self.bensatankin = bensatankin
-        super().__init__(rekisterinimi, huipponopeus)
+class Sähköauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, akkukapasiteetti):
+        super().__init__(rekisteritunnus, huippunopeus)
+        self.akkukapasiteetti = akkukapasiteetti
 
-    def tulosta_tiedot(self):
-        super().tulosta_tiedot()
-        print(f"{self.bensatankin} l")
+    def alusta(self, akkukapasiteetti):
+        self.akkukapasiteetti = akkukapasiteetti
 
-Autot=[]
-Autot.append(sähköauto("ABC-15", 180, 52.5))
-Autot.append(polttomoottoriauto("ACD-123", 165, 32.3))
-for A in Autot:
-    A.tulosta_tiedot()
-#Aseta kummallekin autolle haluamasi nopeus,
-# käske autoja ajamaan kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
+class Polttomoottoriauto(Auto):
+    def __init__(self, rekisteritunnus, huippunopeus, bensatankin_koko):
+        super().__init__(rekisteritunnus, huippunopeus)
+        self.bensatankin_koko = bensatankin_koko
+
+    def alusta(self, bensatankin_koko):
+        self.bensatankin_koko = bensatankin_koko
+
+def main():
+    sähköauto = Sähköauto("ABC-15", 180, 52.5)
+    polttomoottoriauto = Polttomoottoriauto("ACD-123", 165, 32.3)
+
+    sähköauto.alusta(52.5)
+    polttomoottoriauto.alusta(32.3)
+
+    sähköauto.aja(3)
+    polttomoottoriauto.aja(3)
+
+    print(f"Sähköauton matkamittari: {sähköauto.matkamittari} km")
+    print(f"Polttomoottoriauton matkamittari: {polttomoottoriauto.matkamittari} km")
+
+if __name__ == "__main__":
+    main()
